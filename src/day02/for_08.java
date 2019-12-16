01f6bfcb771f0314db12894176464aac09e06985
lc2019 <18192948295@189.cn>
2019-12-16 22:14:28 +0800

    p21

--- src/day02/for_08.java
+++ src/day02/for_08.java
@@ -4,8 +4,10 @@ for的基本格式
  */
 public class for_08 {
     public static void main(String args[]){
-        for (int i = 0; i <10 ; i++) {
-            System.out.println(i);
+        for (int i = 0; i < 20 ; i++) {
+           if (i % 7 ==0 && i % 4 != 0){
+               System.out.println(i);
+           }
         }
     }
 }
