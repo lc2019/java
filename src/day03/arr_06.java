f0f0406a9f7bd3e157c949cbb4da7ab38e41e39b
lc2019 <18192948295@189.cn>
2019-12-19 21:10:30 +0800

    第三天内容

--- src/day03/arr_06.java
+++ src/day03/arr_06.java
@@ -3,9 +3,25 @@ package day03;
 public class arr_06 {
     public static void main(String args[]){
         int[] scores = {59,79,93,100};
-        for (int i = 0; i <scores.length ; i++) {
+        int len = scores.length;
+        for (int i = 0; i <len ; i++) {
             int score = scores[i];
             System.out.println(score);
         }
+
+        //forearch 打印
+        //数组元素类型  变量：数组名
+        for (int x : scores){
+            System.out.println(x);
+        }
+
+        print(scores);
+        }
+        //可变参数作为数组使用
+        public static void print(int ...x){
+            for (int i = 0; i <x.length ; i++) {
+                System.out.println(x[i]);
+        }
+
     }
 }
