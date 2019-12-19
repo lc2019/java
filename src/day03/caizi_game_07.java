22d20e205e6b2365fe5c23166b3c8683f9e6adba
lc2019 <18192948295@189.cn>
2019-12-19 21:10:45 +0800

    第三天内容

---
+++ src/day03/caizi_game_07.java
@@ -0,0 +1,37 @@
+package day03;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class caizi_game_07 {
+    public static void main(String args[]){
+//        int[] nums = {30,17,6,2,22,27};
+
+        int[] nums =  new int[5];
+        int len =  nums.length;
+        //伪随机数
+        Random r = new Random();
+        //猜5次
+        for (int i = 0; i <len ; i++) {
+            nums[i] = r.nextInt(50);
+        }
+
+        Scanner input  = new Scanner(System.in);
+        System.out.println("enter a num，50以内:");
+        int userNum = input.nextInt();
+
+        //标志
+        boolean flag =  false;
+        for (int x:nums) {
+            if (userNum == x){
+                flag = true;
+                break;//结束for循环
+            }
+        }
+        if (flag){
+            System.out.println("right");
+        }else {
+            System.out.println("error");
+        }
+    }
+}
