ba145cafb676109143ff67ff7986eb6f8b65077c
lc2019 <18192948295@189.cn>
2019-12-19 23:53:20 +0800

    day01

--- src/day01/smysf01.java
+++ src/day01/smysf01.java
@@ -2,8 +2,8 @@ package day01;
 
 import java.util.Scanner;
 
-/*
-@lc
+/**
+@author:lc
 三目运算符
  */
 public class smysf01 {
@@ -11,9 +11,7 @@ public class smysf01 {
         System.out.println("score: ");
         Scanner input =  new Scanner(System.in);
         int score = input.nextInt();
-
         //条件表达式？a:b ,条件为真返回a，否则返回b
-
         String s  =score >=60?"及格":"挂科";
         System.out.println(s);
     }
