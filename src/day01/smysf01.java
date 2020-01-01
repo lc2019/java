89843291d9615ca4df2374bfa313f9b212b3d467
lc2019 <18192948295@189.cn>
2020-01-01 23:46:49 +0800

    day01

--- src/day01/smysf01.java
+++ src/day01/smysf01.java
@@ -12,7 +12,13 @@ public class smysf01 {
         Scanner input =  new Scanner(System.in);
         int score = input.nextInt();
         //条件表达式？a:b ,条件为真返回a，否则返回b
-        String s  =score >=60?"及格":"挂科";
+        String s  = score >= 60 ? "及格":"挂科";
         System.out.println(s);
+
+        int a = 100, b = 304,c=289;
+        int max = a>b?a:b;
+//         max = (a>b?a:b)>c?(a>b?a:b):c;
+        max = max>c?max:c;
+        System.out.println(max);
     }
 }
