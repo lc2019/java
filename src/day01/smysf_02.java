94234a3fb90f09ded2b86fd3a5b58eb998313fee
lc2019 <18192948295@189.cn>
2020-04-04 00:37:23 +0800

    04/03

--- src/day01/smysf_02.java
+++ src/day01/smysf_02.java
@@ -3,22 +3,21 @@ package day01;
 import java.util.Scanner;
 
 /**
-@author:lc
-三目运算符
+ * @author:lc 三目运算符
  */
 public class smysf_02 {
     public static void main(String[] args) {
         System.out.println("score: ");
-        Scanner input =  new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int score = input.nextInt();
         //条件表达式？a:b ,条件为真返回a，否则返回b
-        String s  = score >= 60 ? "及格":"挂科";
+        String s = score >= 60 ? "及格" : "挂科";
         System.out.println(s);
 
-        int a = 100, b = 304,c=289;
-        int max = a>b?a:b;
+        int a = 100, b = 304, c = 289;
+        int max = a > b ? a : b;
 //         max = (a>b?a:b)>c?(a>b?a:b):c;
-        max = max>c?max:c;
+        max = max > c ? max : c;
         System.out.println(max);
     }
 }
